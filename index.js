23536f08811785abed3406ea1e36f3e2e2ea6f13
Yidy <jetfung060@gmail.com>
2024-07-15 14:56:53 -0400

    added solution

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here
+const username = 'alice123'
+const password = 'flatironschool'
+let isAuthorized
+if(username === 'alice123' && password ===  'flatironschool') {
+    isAuthorized = true
+}
+else{
+    isAuthorized = false
+}
+const loginValidationPhrase = isAuthorized === true ? 'Welcome ' + username + '!' : 'invalid username or password!'
+const randomNumber = Math.floor(Math.random()* 10)
+const luckyNumberPhrase = `Your lucky number is ${randomNumber}`
